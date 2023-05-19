6ae02296b0e98fac1a3c2309edca40103aa0ca01
Richard Zamora <77524442+drzamora@users.noreply.github.com>
2023-05-19 15:10:56 -0600

    Impide letras mayusculas

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const inputTexto = document.querySelector("#textarea"); /* Hace referencia al id textarea */
-const mensaje = document.querySelector(".mensaje"); /* Hace referencia a la clase mensaje */
-const titulo = document.querySelector(".ocultar_titulo"); /* Hace referencia a la clase ocultar_titulo */
-const parrafo = document.querySelector(".ocultar_parrafo"); /* Hace referencia a la clase ocultar_parrafo */
-const btnCopiar = document.querySelector("#copiar"); /* Hace referencia al id copiar */
+const inputTexto = document.querySelector("#textarea"); // Hace referencia al id textarea 
+const mensaje = document.querySelector(".mensaje"); // Hace referencia a la clase mensaje 
+const titulo = document.querySelector(".ocultar_titulo"); // Hace referencia a la clase ocultar_titulo 
+const parrafo = document.querySelector(".ocultar_parrafo"); // Hace referencia a la clase ocultar_parrafo 
+const btnCopiar = document.querySelector("#copiar"); // Hace referencia al id copiar 
 
 /*  ENCRIPTACION */
-function btnEncriptar(){ /* Funciones para BOTON Encriptar */
+function btnEncriptar(){ // Funciones para boton encriptar 
     const textoEncriptado = codificar(inputTexto.value); 
     mensaje.value = textoEncriptado;
     inputTexto.value = "";
@@ -15,20 +15,20 @@ function btnEncriptar(){ /* Funciones para BOTON Encriptar */
     btnCopiar.style.display = 'block'
 }
 
-function codificar(StringParaEncriptar) { /* Funciones para Encriptar */
+function codificar(StringParaEncriptar) { // Funciones para Encriptar 
     let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]]
     StringParaEncriptar = StringParaEncriptar.toLowerCase();
 
-    for(let i=0; i < matrizCodigo.length; i++){ /* Recorre la matriz */
+    for(let i=0; i < matrizCodigo.length; i++){ // Recorre la matriz 
         if(StringParaEncriptar.includes(matrizCodigo[i][0])){
-            StringParaEncriptar = StringParaEncriptar.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]) /* Realiza el cambio de caracteres */
+            StringParaEncriptar = StringParaEncriptar.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]) // Realiza el cambio de caracteres 
         }
     }
     return StringParaEncriptar;
 } 
 
 /* DESENCRIPTACION */
-function btnDesencriptar(){ /* Funciones para BOTON Encriptar */
+function btnDesencriptar(){ // Funciones para boton desencriptar 
     const textoEncriptado = decodificar(inputTexto.value); 
     mensaje.value = textoEncriptado;
     inputTexto.value = "";
@@ -38,19 +38,19 @@ function btnDesencriptar(){ /* Funciones para BOTON Encriptar */
     btnCopiar.style.display = 'block'
 }
 
-function decodificar(StringParaEncriptar) { /* Funciones para desencriptar */
+function decodificar(StringParaEncriptar) { // Funciones para desencriptar 
     let matrizCodigo = [["enter","e"],["imes","i"],["ai","a"],["ober","o"],["ufat","u"]]
     StringParaEncriptar = StringParaEncriptar.toLowerCase();
 
-    for(let i=0; i < matrizCodigo.length; i++){ /* Recorre la matriz */
+    for(let i=0; i < matrizCodigo.length; i++){ // Recorre la matriz 
         if(StringParaEncriptar.includes(matrizCodigo[i][0])){
-            StringParaEncriptar = StringParaEncriptar.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]) /* Realiza el cambio de caracteres */
+            StringParaEncriptar = StringParaEncriptar.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]) // Realiza el cambio de caracteres 
         }
     }
     return StringParaEncriptar;
 } 
 
-/* COPIAR*/
+/* COPIAR */
 function copiarTexto() { 
     var content = document.getElementById('resultado');
     content.select();
